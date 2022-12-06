7c3c1ff11c2ed7fbe819afbe00c54839adf0c1b9
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-06 16:29:33 -0500

    Add project files.

---
+++ WebApplication/Models/ErrorViewModel.fs
@@ -0,0 +1,8 @@
+namespace WebApplication
+
+open System
+
+type ErrorViewModel private () =
+    member val RequestId : string = null with get, set
+
+    member val ShowRequestId : bool = true with get, set
---
+++ WebApplication/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+// Functions to be called onmouseover and onmouseout to change the color of "Hello, World"
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+// functions to be called onmouseover and onmouseout to mainpulate div myAnimation
+function newText(obj) {
+    obj.innerHTML = "Thanks!"
+}
+
+function oldText(obj) {
+    obj.innerHTML = "Mouse over me!"
+}
